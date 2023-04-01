5dca90326798855b69db305b34ae7d281cb19dd9
Nkita <kablovnv@gmail.com>
2023-04-01 18:20:20 +0300

    решил вторую, но чтото не так

---
+++ HomeWork1/task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру
+// заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int number3 = number % 10;
+if (number3 == 0) 
+{
+     Console.Write("нет третий цыфры: ");
+}
+else {
+    Console.Write($"Третья цыфра числа {number3} ");
+}
+
+// int ReadInt(string message)
+// {
+//     Console.Write(message);
+//     int value = Convert.ToInt32(Console.ReadLine());
+//     return value;
+// }
+// int number = ReadInt("Введите число: ");
+// int LastDigit = number % 10;
+// Console.WriteLine($"последняя цифра числа {number} равна {LastDigit}");
