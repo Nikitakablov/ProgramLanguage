8726062124f8970ad647d335c21469e9eaaaf1c9
Nkita <kablovnv@gmail.com>
2023-04-02 21:15:45 +0300

    задача 3

---
+++ HomeWork1/task3/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая принимает на 
+// вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным. // 6 -> да
+ // 1 -> нет
+int ReadInt(string message)
+{
+    Console.Write(message);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
