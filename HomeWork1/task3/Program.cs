febf4976bd3da677f1d18346b760066f3a2c20ab
Nkita <kablovnv@gmail.com>
2023-04-04 18:34:50 +0300

    решена 3 задачу дни неднли

--- HomeWork1/task3/Program.cs
+++ HomeWork1/task3/Program.cs
@@ -1,10 +1,26 @@
-﻿//  Напишите программу, которая принимает на 
-// вход цифру, обозначающую день недели,
-// и проверяет, является ли этот день выходным. // 6 -> да
- // 1 -> нет
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+//является ли этот день выходным.
+Console.Clear();
 int ReadInt(string message)
 {
     Console.Write(message);
-    int value = Convert.ToInt32(Console.ReadLine());
-    return value;
+    return Convert.ToInt32(Console.ReadLine());
+}
+bool Weekend(int weekday)
+{
+    if (weekday > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+int weekday = ReadInt("Введите номер дня недели: ");
+if (Weekend(weekday))
+{
+    Console.WriteLine("Это выходной день");
+}
+else
+{
+    Console.WriteLine("Это рабочий день");
 }
