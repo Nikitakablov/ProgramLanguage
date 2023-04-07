6b395e62ae7b3a9a0a1c4a4b70fdfa3ed755d2c0
Nkita <kablovnv@gmail.com>
2023-04-07 23:01:10 +0300

    решил задачу № 3

---
+++ HomeWork3/task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write($"{message}"); // ввод приглашения
+    return Convert.ToInt32(Console.ReadLine()); // ввод числа
+}
+int N = ReadInt("Введите число: ");
+for(int i = 1; i<=N; i++)
+{
+   Console.Write($"{Math.Pow(i,3)}; ");
+}
