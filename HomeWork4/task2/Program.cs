22cca18c027ac5bbe2beff5fd7311dda2c39ad57
Nkita <kablovnv@gmail.com>
2023-04-08 12:07:37 +0300

    задача 2

---
+++ HomeWork4/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int SumDigit(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        result = result + digit;
+        num/= 10;
+    }
+    return result;
+}
+int num = ReadInt("Введите число: ");
+Console.WriteLine($"сумма цифр в числе {num} равна {SumDigit(num)} ");
