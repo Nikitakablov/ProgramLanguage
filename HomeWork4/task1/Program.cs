f94a80ee3466b1f527e24d39243a63089b0dc7e4
Nkita <kablovnv@gmail.com>
2023-04-08 11:50:31 +0300

    задача 1

---
+++ HomeWork4/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Involution(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+    return result;
+}
+int A = ReadInt("Введите число А: ");
+int B = ReadInt("Введите число В: ");
+Console.WriteLine($"{A} в степени {B} равно {Involution(A, B)}");
+
+
