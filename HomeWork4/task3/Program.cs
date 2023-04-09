bdfc3cd57fc65d69ab511399bb5c1f25bc9f9f5b
Nkita <kablovnv@gmail.com>
2023-04-09 12:32:27 +0300

    задача 3

---
+++ HomeWork4/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+Console.Clear();
+int[] CreatArray()
+{
+    int[] res = new int[8];
+    for(int i = 0; i < res.Length; i++)
+    {
+        res[i] = new Random().Next(0,101);
+    }
+    return res;
+}
+void PrintArray(int[]array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+Console.WriteLine();
+}
+int[] massiv = CreatArray();
+PrintArray (massiv);
+
