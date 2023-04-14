33b3ff8b2bedfcf49438b1b33be73a9e64f628c3
Nkita <kablovnv@gmail.com>
2023-04-14 21:46:51 +0300

    задача 2

---
+++ HomeWork5/task2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+Console.Clear();
+int[] CreatArray()
+{
+    int[] array = new int[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-99, 100);
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t"); // \t табуляция мас
+    }
+    Console.WriteLine();
+}
+int SumElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+int[] array = CreatArray();
+PrintArray(array);
+int sum = SumElement(array);
+Console.WriteLine($"Сумма элементов на нечетных позициях: {sum} ");
+
