512df687e88f7be595c3b5b587e13e98269104ed
Nkita <kablovnv@gmail.com>
2023-04-15 20:44:59 +0300

    задача 3

---
+++ Lasson6/task3/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write($"{message} ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] Binary(int num)
+{
+    int[] array = new int[10];
+    for (int i = array.Length - 1; i >= 0; i--)
+    {
+        array[i] = num % 2;
+        num/=2;
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} "); // \t табуляция мас
+    }
+    Console.WriteLine();
+}
+int num = ReadInt("Введите число ");
+int [] massiv = Binary(num);
+PrintArray(massiv);
+// PrintArray(Binary(num));
+
