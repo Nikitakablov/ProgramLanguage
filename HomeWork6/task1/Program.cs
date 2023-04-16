cc08ec91c084743e05bf19e8dc51eaaa24345430
Nkita <kablovnv@gmail.com>
2023-04-16 17:36:33 +0300

    решил две задачи, но во второй не разобрался до конца

---
+++ HomeWork6/task1/Program.cs
@@ -0,0 +1,52 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+Console.Clear();
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] CreatArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt($"Введите {i + 1} - элемент");
+    }
+    return array;
+}
+// int[] CreatArray()
+// {
+//     int[] array = new int[4];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(-99, 100);
+//     }
+//     return array;
+// }
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}\t"); // \t табуляция мас
+    }
+    Console.WriteLine();
+}
+int Big0(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length = ReadInt("Введите кол-во элементов - ");
+int[] array = CreatArray(length);
+PrintArray(array);
+int cnt = Big0(array);
+Console.WriteLine($"кол-во чисел больше 0 равно = {cnt} ");
---
+++ HomeWork6/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.Clear();
+double ReadInt(string message)
+{
+    Console.Write($"{message} > "); // ввод приглашения
+    return Convert.ToDouble(Console.ReadLine()); // ввод числа
+}
+void Kor()
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения прямых: ({x}; {y})");
+}
+double b1 = ReadInt("Введите значение b1 ");
+double k1 = ReadInt("Введите значение k1 ");
+double b2 = ReadInt("Введите значение b2 ");
+double k2 = ReadInt("Введите значение k2 ");
+Kor();
+
+
+
+
