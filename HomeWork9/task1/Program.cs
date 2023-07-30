505458d147ccf64378ea161921460f625ed5339d
Nkita <kablovnv@gmail.com>
2023-07-30 19:31:36 +0300

    решена первая задача

---
+++ HomeWork9/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задайте значения M и N. Напишите программу, которая выведет все 
+//чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+Console.Clear();
+int ReadInt(string massage)
+{
+   Console.Write($"{massage} > ");
+   return int.Parse(Console.ReadLine());
+}
+void EvenNumbers(int current, int N)
+{
+    if (current > N)
+    return;
+    if (current % 2 == 0)
+    {
+        System.Console.WriteLine(current);
+    }
+    EvenNumbers(current + 1,N);
+}
+int M = ReadInt("Введите число M: ");
+int N = ReadInt("Введите число N: ");
+EvenNumbers(M, N);
